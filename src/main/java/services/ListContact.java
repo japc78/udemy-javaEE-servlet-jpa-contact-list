9fa051fc92be4d2e023f6737ab21da144bf5217f
japc <japc.grafico@gmail.com>
2021-08-29 20:32:39 +0200

    Update WebServlet

--- src/main/java/services/ListContact.java
+++ src/main/java/services/ListContact.java
@@ -12,7 +12,7 @@ import model.ContactList;
 /**
  * Servlet implementation class ContactList
  */
-@WebServlet("/ContactList")
+@WebServlet("/ListContact")
 public class ListContact extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
