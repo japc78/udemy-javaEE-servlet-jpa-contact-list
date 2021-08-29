e68c6183a94045d8c3e7c84c204dd8d6b20a92e8
japc <japc.grafico@gmail.com>
2021-08-29 13:15:15 +0200

    Add method addContact

--- src/main/java/model/ContactList.java
+++ src/main/java/model/ContactList.java
@@ -1,17 +1,48 @@
 package model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import beans.Contact;
 import tools.ReadProperties;
 
 public class ContactList {
 	private static ReadProperties config = new ReadProperties();
 	private static String driver = config.getDriver();
-
+	private String url = config.getUrl();
+	private String user = config.getUser();
+	private String pass = config.getPass();
+	
 	static {
 		try {
 			Class.forName(driver);
 
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
+		}
+	}
+	
+	public void addContact(Contact contact) {
+		try (Connection cn = DriverManager.getConnection(url, user, pass);) {
+
+			String sql = "Insert into contactos (name, email, phoneNumber)";
+			sql += "values(?, ?, ?)";
+
+			// Make query
+			PreparedStatement ps = cn.prepareStatement(sql);
+
+			// Pass values
+			ps.setString(1, contact.getName());
+			ps.setString(2, contact.getEmail());
+			ps.setInt(3, contact.getNumberPhone());
+
+			// Run query
+			ps.execute();
+
+		} catch (SQLException ex) {
+			ex.printStackTrace();
 		}
 	}
 }
